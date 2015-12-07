6249e874fcd100d2f4b77c65ffea1ae33fb2f12f
Jim Remsik <jim@adorable.io>
2015-12-07 15:51:11 -0600

    Require compression

--- app/scripts/web.js
+++ app/scripts/web.js
@@ -1,7 +1,9 @@
 'use strict';
 
+var compression = require('compression');
 var express = require('express');
 var nodeApp = express();
 
 nodeApp.use(express.logger('dev'));
+nodeApp.use(compression());
 nodeApp.listen(process.env.PORT || 5000);
