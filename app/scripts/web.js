8eb0cceb11fc381a7d78e80547081f0015583bc6
Jim Remsik <jim@adorable.io>
2015-12-07 16:09:29 -0600

    RTFM n00b

--- app/scripts/web.js
+++ app/scripts/web.js
@@ -4,6 +4,6 @@ var compression = require('compression');
 var express = require('express');
 var nodeApp = express();
 
-nodeApp.use(express.logger('dev'));
+// nodeApp.use(express.logger('dev'));
 nodeApp.use(compression());
 nodeApp.listen(process.env.PORT || 5000);
